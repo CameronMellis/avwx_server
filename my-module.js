fd21ae2848ffccee22cf16944aecab23818628bc
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-05-28 17:12:00 -0230

    basic express

--- my-module.js
+++ my-module.js
@@ -1,3 +1,3 @@
-module.exports = {
-    hello: 'world'
-}
+// module.exports = {
+//   hello: 'world'
+// }
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(loggingMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.get('/users', (req, res) => {
+  res.send('Users Page');
+});
+
+function loggingMiddleware(req, res, next) {
+  console.log('Inside loggingMiddleware');
+  next();
+}
+
+app.listen(3000, () => console.log('Server Started'));
