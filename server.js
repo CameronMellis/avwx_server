4e986b4f416fe11f9e2fdada9fc62267f65df5f5
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-05-30 21:42:40 -0230

    testing

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-  res.send('Users Page');
+  res.send('Welcome to the twilight zone');
 });
 
 function loggingMiddleware(req, res, next) {
-  console.log('${new Date().toISOString()}: ${req.originalUrl}');
+  console.log('Hi Mom');
   next();
 }
-
 
 
 app.listen(3000, () => console.log('Server Started'));
