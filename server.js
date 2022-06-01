9c263598feb8ae5e16784f3e4e4bdeeeed96ef25
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-05-31 22:27:59 -0230

    express testing

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-res.render('index')
+  res.render('index', { text: 'Hello World' });
+});
+
+app.get('/users', (req, res) => {
+  res.send('User List');
+});
+
+app.get('/users/new', (req, res) => {
+  res.send('New User Form');
 });
 
 app.listen(3000);
-
