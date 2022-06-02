7e240314ac9b14407bea75026b7e84d87d3f6527
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-06-02 16:52:00 -0230

    api call trial success

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+request(
+  {
+    method: 'GET',
+    url: 'https://avwx.rest/api/metar/YYT',
+    headers: {
+      Authorization: 'dB2w9VHe5mzuqKLgd9Slf5RyIg5jUv3mNV5DyWJpY4s',
+    },
+  },
+  function (error, response, body) {
+    console.log('Status:', response.statusCode);
+    console.log('Headers:', JSON.stringify(response.headers));
+    console.log('Response:', body);
+  }
+);
 
 app.get('/', (req, res) => {
   res.render('index', { text: 'Can you hear me now?' });
