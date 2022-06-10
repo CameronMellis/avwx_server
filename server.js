b3b89ce08df32d22c022deb8ce9067f39218be95
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-06-09 23:30:50 -0230

    multiple GET API requests success

--- server.js
+++ server.js
@@ -5,24 +5,59 @@ const request = require('request');
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  request(
-    {
-      method: 'GET',
-      url: 'https://avwx.rest/api/metar/YYT',
-      headers: {
-        Authorization: 'dB2w9VHe5mzuqKLgd9Slf5RyIg5jUv3mNV5DyWJpY4s',
-      },
-    },
-    function (error, response, body) {
-      console.log('Status:', response.statusCode);
-      console.log('Headers:', JSON.stringify(response.headers));
-      console.log('Response:', body);
-      let wxdata = JSON.parse(body);
-      res.render('index', { wxdata });
-    }
-  );
-});
+var requestAsync = function (url) {
+  return new Promise((resolve, reject) => {
+    var req = request(url, headers, (err, response, body) => {
+      if (err) return reject(err, response, body);
+      resolve(JSON.parse(body));
+    });
+  });
+};
+
+const urls = [
+  'https://avwx.rest/api/metar/CYYT',
+  'https://avwx.rest/api/taf/CYYT',
+  'https://avwx.rest/api/metar/CYQX',
+  'https://avwx.rest/api/taf/CYQX',
+  'https://avwx.rest/api/metar/CYDF',
+  'https://avwx.rest/api/taf/CYDF',
+  'https://avwx.rest/api/metar/CYJT',
+  'https://avwx.rest/api/taf/CYJT',
+  'https://avwx.rest/api/metar/CYAY',
+  'https://avwx.rest/api/taf/CYAY',
+  'https://avwx.rest/api/metar/CYBX',
+  'https://avwx.rest/api/taf/CYBX',
+  'https://avwx.rest/api/metar/CYYR',
+  'https://avwx.rest/api/taf/CYYR',
+  'https://avwx.rest/api/metar/CZUM',
+  'https://avwx.rest/api/taf/CZUM',
+  'https://avwx.rest/api/metar/CYWK',
+  'https://avwx.rest/api/taf/CYWK',
+  'https://avwx.rest/api/metar/CYDP',
+  'https://avwx.rest/api/taf/CYDP',
+  'https://avwx.rest/api/metar/CYNA',
+  'https://avwx.rest/api/taf/CYNA',
+  'https://avwx.rest/api/metar/CYKL',
+  'https://avwx.rest/api/taf/CYKL',
+];
+
+const headers = {
+  headers: {
+    Authorization: 'dB2w9VHe5mzuqKLgd9Slf5RyIg5jUv3mNV5DyWJpY4s',
+  },
+};
+
+var getData = async function () {
+  //transform requests into Promises, await all
+  try {
+    var data = await Promise.all(urls.map(requestAsync));
+  } catch (err) {
+    console.error(err);
+  }
+  console.log(data);
+};
+
+getData();
 
 const userRouter = require('./routes/users');
 
