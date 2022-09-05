afd76cf06581be306320b0a6989295ffe0b818ff
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-09-05 20:33:34 -0230

    https

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const wxservice = require('./wxservice');
 const cron = require('node-cron');
 const userRouter = require('./routes/server_routes');
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 
 //Sketchy work around for dev build, fix for production
 
@@ -16,9 +18,23 @@ app.use(bodyParser.json());
 
 app.use(userRouter);
 
-wxservice.getData().then(() => {
-  app.listen(3001);
-});
+https
+  .createServer(
+    // Provide the private and public key to the server by reading each
+    // file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(3001, () => {
+    wxservice.getData();
+  });
+
+// wxservice.getData().then(() => {
+//   app.listen(3001);
+// });
 
 // cron currently updating wxdata every hour, extra requests saved for manual updating
 
