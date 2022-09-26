d797d4e2c9fa6972932e63e8dd284701d625a5a9
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-09-26 21:25:30 -0230

    yolo again

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const wxservice = require('./wxservice');
-const cron = require('node-cron');
-const userRouter = require('./routes/server_routes');
-const bodyParser = require('body-parser');
-const path = require('path');
+// const cors = require('cors');
+// const wxservice = require('./wxservice');
+// const cron = require('node-cron');
+// const userRouter = require('./routes/server_routes');
+// const bodyParser = require('body-parser');
+// const path = require('path');
 // const https = require('https');
-const fs = require('fs');
+// const fs = require('fs');
 
 //Sketchy work around for dev build, fix for production
 
-app.use(cors());
+// app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use(userRouter);
+// app.use(userRouter);
 
 // add middleware for static server
 
@@ -48,14 +48,14 @@ app.listen(3000, () => {
 
 // cron currently updating wxdata every hour, extra requests saved for manual updating
 
-cron.schedule(
-  '0 * * * *',
-  () => {
-    wxservice.getData();
-    console.log('getting new weather every hour');
-  },
-  {
-    scheduled: true,
-    timezone: 'UTC',
-  }
-);
+// cron.schedule(
+//   '0 * * * *',
+//   () => {
+//     wxservice.getData();
+//     console.log('getting new weather every hour');
+//   },
+//   {
+//     scheduled: true,
+//     timezone: 'UTC',
+//   }
+// );
