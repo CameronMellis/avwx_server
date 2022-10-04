e7287ebf996ca80c6a8cee4232d033e7e2865517
CameronMellis <88047548+CaMel-315@users.noreply.github.com>
2022-10-04 20:52:00 -0230

    new dist

--- server.js
+++ server.js
@@ -22,12 +22,8 @@ app.use(userRouter);
 
 // add middleware for static server
 
-app.use('/', express.static(path.join(__dirname, 'dist2')));
-console.log(path.join(__dirname, 'dist2'));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
+app.use('/', express.static(path.join(__dirname, 'dist')));
+console.log(path.join(__dirname, 'dist'));
 
 app.listen(port, () => {
   console.log('Server is running on port 3000');
